a5b2d29102c8c559b4164de536c7291ca6a93e0a
jimby3 <jimever333@gmail.com>
2024-10-13 16:06:07 -0600

    very basic prototype for front end

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { AboutComponent } from './pages/about/about.component';
+import { AssignmentsComponent } from "./pages/assignments/assignments.component";
+import { CalendarComponent } from "./pages/calendar/calendar.component";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'assignments', component: AssignmentsComponent },
+  { path: 'calendar', component: CalendarComponent },
+  { path: 'about', component: AboutComponent },
+
+];
