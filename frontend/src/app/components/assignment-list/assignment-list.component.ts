1422e74128b16e25d2f9105b4b1b31c951098812
jimby3 <jimever333@gmail.com>
2024-10-14 15:25:57 -0600

    very basic prototype for front end

---
+++ frontend/src/app/components/assignment-list/assignment-list.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-assignment-list',
+  standalone: true,
+  imports: [],
+  templateUrl: './assignment-list.component.html',
+  styleUrl: './assignment-list.component.css'
+})
+export class AssignmentListComponent {
+
+}
