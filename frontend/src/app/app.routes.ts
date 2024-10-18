8ba53d9e6ecf6b53f5cb4b4241be7887ff082911
jimby3 <jimever333@gmail.com>
2024-10-18 15:51:38 -0600

    make login page part of navbar

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,9 +1,9 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { AboutComponent } from './pages/about/about.component';
-import { AssignmentsComponent } from "./pages/assignments/assignments.component";
-import { CalendarComponent } from "./pages/calendar/calendar.component";
-import {LoginComponent} from "./pages/login/login.component";
+import { AssignmentsComponent } from './pages/assignments/assignments.component';
+import { CalendarComponent } from './pages/calendar/calendar.component';
+import { LoginComponent } from './pages/login/login.component';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },
@@ -11,5 +11,4 @@ export const routes: Routes = [
   { path: 'calendar', component: CalendarComponent },
   { path: 'about', component: AboutComponent },
   { path: 'login', component: LoginComponent },
-
 ];
--- frontend/src/app/pages/login/login.component.ts
+++ frontend/src/app/pages/login/login.component.ts
@@ -1,26 +1,29 @@
 import { Component } from '@angular/core';
 import { AuthService } from '../../components/auth.service';
-import {FormsModule} from "@angular/forms";
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   standalone: true,
   imports: [
-    FormsModule
+    FormsModule,
   ],
   templateUrl: './login.component.html'
 })
 export class LoginComponent {
-  username: string = '';
-  password: string = '';
 
-  constructor(private authService: AuthService) {}
+  // commented out for now
 
-  login() {
-    this.authService.login(this.username, this.password).subscribe(response => {
-      // Handle successful login
-    }, error => {
-      // Handle login error
-    });
-  }
+  // username: string = '';
+  // password: string = '';
+  //
+  // constructor(private authService: AuthService) {}
+  //
+  // login() {
+  //   this.authService.login(this.username, this.password).subscribe(response => {
+  //     // Handle successful login
+  //   }, error => {
+  //     // Handle login error
+  //   });
+  // }
 }
