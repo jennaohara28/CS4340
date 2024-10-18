908265f23d05b339ddf7868e7ec76ea1a692557f
jimby3 <jimever333@gmail.com>
2024-10-18 15:43:16 -0600

    make login page part of navbar

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -3,11 +3,13 @@ import { HomeComponent } from './pages/home/home.component';
 import { AboutComponent } from './pages/about/about.component';
 import { AssignmentsComponent } from "./pages/assignments/assignments.component";
 import { CalendarComponent } from "./pages/calendar/calendar.component";
+import {LoginComponent} from "./pages/login/login.component";
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'assignments', component: AssignmentsComponent },
   { path: 'calendar', component: CalendarComponent },
   { path: 'about', component: AboutComponent },
+  { path: 'login', component: LoginComponent },
 
 ];
--- frontend/src/app/pages/login/login.component.ts
+++ frontend/src/app/pages/login/login.component.ts
@@ -1,9 +1,13 @@
 import { Component } from '@angular/core';
 import { AuthService } from '../../components/auth.service';
+import {FormsModule} from "@angular/forms";
 
 @Component({
   selector: 'app-login',
   standalone: true,
+  imports: [
+    FormsModule
+  ],
   templateUrl: './login.component.html'
 })
 export class LoginComponent {
