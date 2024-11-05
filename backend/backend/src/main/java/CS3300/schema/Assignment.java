a106620aad28e14a989e944724605e18b655bf3c
Jenna O'Hara <jennagutier9@gmail.com>
2024-11-04 19:52:52 -0700

    Created Class and Task objects for backend

--- backend/backend/src/main/java/CS3300/schema/Assignment.java
+++
@@ -1,4 +0,0 @@
-package CS3300.schema;
-
-public class Assignment {
-}
--- backend/backend/src/main/java/CS3300/schema/Class.java
+++ backend/backend/src/main/java/CS3300/schema/Class.java
@@ -1,4 +1,42 @@
 package CS3300.schema;
 
+import java.util.ArrayList;
+
 public class Class {
+
+    private String name;
+    private ArrayList<Task> tasks;
+    private String color;
+
+    public Class(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getColor() {
+        return color;
+    }
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public void displayTasks() {
+        for (Task task : tasks) {
+            System.out.println(task.name);
+        }
+    }
+
+    public void addTask(Task toAdd){
+        tasks.add(toAdd);
+    }
+    public void removeTask(Task toRemove){
+        tasks.remove(toRemove);
+    }
+
 }
