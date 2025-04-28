51f64ba8642b4176055b122978040c8546702a91
Jimmy O'Hara <123116539+Jimby3@users.noreply.github.com>
2025-04-27 20:01:35 -0600

    changing IP

--- mobile-frontend/api/client.js
+++ mobile-frontend/api/client.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://76.155.212.102:8080', //
+    // change to 76.155.212.102 for external ip/demo
+    baseURL: 'http://10.0.0.195:8080', //
     headers: {
         'Content-Type': 'application/json',
     },
